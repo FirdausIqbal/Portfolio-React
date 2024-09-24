aec67d6b2e746fa64dae8287a2e14989a17d1c1e
FirdausIqbal <firdausiqbal10@gmail.com>
2024-09-25 00:27:55 +0700

    fix padding responsive on banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -18,6 +18,9 @@ const InnerText = styled.div`
   align-items: center;
   justify-content: center;
   padding: 0 4rem;
+  ${mobile({
+    padding: '0 1rem'
+  })}
 `;
 const Main = styled.div`
   font-size: 3.3rem;
